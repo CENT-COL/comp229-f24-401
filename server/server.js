bc0e8eb60709ed118e5ed481332d19ef00505d53
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2024-12-03 13:34:24 +0000

    added deploy code

--- server/server.js
+++ server/server.js
@@ -141,6 +141,15 @@ app.use('/api/data', (req, res) => {
     res.json({message: 'Hello from server'});
 })
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+    
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+    });
+  }
+
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
