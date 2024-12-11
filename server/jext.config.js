d590cd1d23a354445737ea262e004b5537d783c7
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2024-12-11 09:58:31 -0500

    added mocked data

--- server/jext.config.js
+++ server/jext.config.js
@@ -1,4 +1,5 @@
 // Jest configuration file path: server/jest.config.js
 module.exports = {
     testEnvironment: 'node',
+    testMatch: ['**/tests/**/*.test.js'], // Look for test files in the tests directory
 }
--- server/tests/projects.test.js
+++ server/tests/projects.test.js
@@ -1,11 +1,22 @@
 // Description: Test cases for projects routes. Path: server/tests/projects.test.js
+const mockingoose = require('mockingoose');
+const Project = require('../models/projects');
 const request  = require('supertest');
 const app = require('../app');
 
 describe('GET /api/projects', () => {
     it("should return a list of projects", async () => {
+
+        const mockProjects = [
+            {_id: '1', name: 'Project 1', description: 'Description 1', startDate: '2021-01-01', endDate: '2021-01-31'},
+            {_id: '2', name: 'Project 2', description: 'Description 2', startDate: '2021-02-01', endDate: '2021-02-28'},
+        ];
+
+        mockingoose.Project.toReturn(mockProjects, 'find');
+
         const response = await request(app).get('/api/projects');
         expect(response.status).toBe(200);
         expect(response.body).toBeInstanceOf(Array);
+        expect(response.body).toHaveLength(2);
     });
 });
